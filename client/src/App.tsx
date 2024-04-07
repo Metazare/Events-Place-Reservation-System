9f97e3c700d806fd002b3b00284955691bf87518
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-04-08 04:34:35 +0800

    feat: set up sockets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import io from 'socket.io-client';
 
 // Layouts
 import Base from './Layouts/Base/Base';
@@ -11,9 +12,13 @@ import Register from './Pages/Register/Register';
 
 // Test
 import TestHelpdesk from './Test/TestHelpdesk';
+import TestNotification from './Test/TestNotification';
 
 // Hooks
 import { ProtectedRoute } from './Hooks/useAuth';
+
+
+const socket = io(process.env.REACT_APP_API_URL || 'http://localhost:5000');
 
 function App() {
   return (
@@ -25,6 +30,7 @@ function App() {
         </Route>
 
         <Route path="/test/helpdesk" element={<TestHelpdesk/>} />
+        <Route path="/test/notification" element={<TestNotification socket={socket}/>} />
 
         {/* Sample usage of Protected Route */}
         <Route element={<ProtectedRoute allowedRoles={["admin"]}/>}>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
@@ -14,6 +16,9 @@ import authRoute from './api/auth/auth.route';
 import helpdeskRoute from './api/helpdesk/helpdesk.route';
 import notificationRoute from './api/notification/notification.route';
 
+import { createNotification } from './api/notification/notification.controller';
+import { CreateNotification } from './api/notification/notification.types';
+
 // Utilities
 import { NotFound } from './utilities/errors';
 import envs from './utilities/envs';
@@ -22,6 +27,13 @@ import envs from './utilities/envs';
 const { PORT, MONGO_URI, CORS_ORIGIN } = envs;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: CORS_ORIGIN,
+        credentials: true
+    }
+});
 
 app.use(cors({ credentials: true, origin: CORS_ORIGIN }));
 app.use(cookieParser());
@@ -35,6 +47,22 @@ app.use('/notification', notificationRoute);
 
 app.use((_req, _res, next) => next(new NotFound()));
 app.use(errorHandler);
+
+io.on('connection', (socket) => {
+    socket.on('notification', async (req: CreateNotification) => {
+        await createNotification(req);
+        io.emit('notification', req);
+    });
+
+    // socket.on('chat', async (req: CreateMessage) => {
+    //     await createMessage(req);
+    //     io.emit('chat', req);
+    // });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 mongoose
     .connect(MONGO_URI)
