20111b2fb0d65817e0c7f936b6d378c9e38970fe
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-04-08 03:05:49 +0800

    test: report, respond, & view one or all helpdesk entry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,9 @@ import Default from './Pages/LandingPage/Default';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 
+// Test
+import TestHelpdesk from './Test/TestHelpdesk';
+
 // Hooks
 import { ProtectedRoute } from './Hooks/useAuth';
 
@@ -20,6 +23,8 @@ function App() {
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
         </Route>
+
+        <Route path="/test/helpdesk" element={<TestHelpdesk/>} />
 
         {/* Sample usage of Protected Route */}
         <Route element={<ProtectedRoute allowedRoles={["admin"]}/>}>
