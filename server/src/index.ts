ecbd5297cdc66cbf0faddb845fc6257f819ef69e
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-04-09 18:02:36 +0800

    refactor: index to include socket, chat route, & db changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,7 @@ import errorHandler from './middlewares/errorHandler';
 import authRoute from './api/auth/auth.route';
 import helpdeskRoute from './api/helpdesk/helpdesk.route';
 import notificationRoute from './api/notification/notification.route';
+import chatRoute from './api/chat/message.route';
 
 import { createNotification } from './api/notification/notification.controller';
 import { CreateNotification } from './api/notification/notification.types';
@@ -22,18 +23,11 @@ import { CreateNotification } from './api/notification/notification.types';
 // Utilities
 import { NotFound } from './utilities/errors';
 import envs from './utilities/envs';
+import connectToMongoDB from "./database/connectToMongoDB";
+import { app, server } from "./socket/socket";
 
 // Environment Variables
 const { PORT, MONGO_URI, CORS_ORIGIN } = envs;
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: CORS_ORIGIN,
-        credentials: true
-    }
-});
 
 app.use(cors({ credentials: true, origin: CORS_ORIGIN }));
 app.use(cookieParser());
@@ -44,30 +38,12 @@ app.use('/auth', authRoute);
 app.use(authenticate);
 app.use('/helpdesk', helpdeskRoute);
 app.use('/notification', notificationRoute);
+app.use('/chat', chatRoute);
 
 app.use((_req, _res, next) => next(new NotFound()));
 app.use(errorHandler);
 
-io.on('connection', (socket) => {
-    socket.on('notification', async (req: CreateNotification) => {
-        await createNotification(req);
-        io.emit('notification', req);
-    });
-
-    // socket.on('chat', async (req: CreateMessage) => {
-    //     await createMessage(req);
-    //     io.emit('chat', req);
-    // });
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+server.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
 });
-
-mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-        console.log('Connected to database');
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-    })
-    .catch(console.error);
