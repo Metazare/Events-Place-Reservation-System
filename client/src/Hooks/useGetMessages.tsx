5367cc0ab4afa279e54154e828718a44a351a883
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-04-09 22:26:38 +0800

    feat: create message related hooks

---
+++ client/src/Hooks/useGetMessages.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import useConversation from "../Zustand/useConversation";
+import toast from "react-hot-toast";
+import axios from './useAxios';
+
+interface Message {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  message: string;
+}
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoading(true);
+      try {
+        await axios
+          .get("/chat/users", {
+            params: {
+              conversationId: selectedConversation?._id,
+            },
+          })
+          .then((response: any) => {
+            setMessages(response.data);
+            
+          });
+
+        const res = await fetch(`/api/messages/${selectedConversation?._id}`);
+        const data: Message[] = await res.json();
+      } catch (error: any) {
+        toast.error((error as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (selectedConversation?._id) getMessages();
+  }, [selectedConversation?._id, setMessages]);
+
+  return { messages, loading };
+};
+
+export default useGetMessages;
---
+++ client/src/Hooks/useSendMessage.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import useConversation from "../Zustand/useConversation";
+import toast from "react-hot-toast";
+import axios from './useAxios';
+
+interface Message {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  message: string;
+}
+
+const useSendMessage = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const { messages, setMessages, selectedConversation } = useConversation();
+
+    const sendMessage = async (message: string) => {
+        setLoading(true);
+        console.log(message)
+        try {
+            await axios
+                .post("/chat/send", { 
+                    receiverId: selectedConversation?._id,
+                    message: message
+                })
+                .then((response: any) => {
+                    console.log(response.data);
+                    setMessages([...messages, response.data]);
+                });
+        } catch (error: any) {
+            console.log(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { sendMessage, loading };
+};
+
+export default useSendMessage;
