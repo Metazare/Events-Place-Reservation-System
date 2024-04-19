f33a21ab0abae95fcd3833cbf1481bdbad26464b
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-04-19 22:19:10 +0800

    feat(auth): otp generator hook

---
+++ client/src/Hooks/useOTPGenerator.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import useEmail from './useEmail';
+
+const useOTPGenerator = () => {
+    const [otp, setOTP] = useState('');
+    const { sendEmail } = useEmail();
+
+    const generateOTP = (email: string) => {
+        const generatedOTP = Math.floor(1000 + Math.random() * 9000).toString();
+        setOTP(generatedOTP);
+
+        sendEmail({
+            to: email,
+            subject: "Events Place Marikina OTP Code",
+            content: `Your OTP code is: ${generatedOTP}`,
+        });
+    };
+
+    const compareOTP = (inputOTP: string) => {
+        return otp === inputOTP;
+    };
+
+    return { generateOTP, compareOTP };
+};
+
+export default useOTPGenerator;
