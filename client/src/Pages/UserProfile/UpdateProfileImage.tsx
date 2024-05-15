699f593df1e0d3ff1ae2319f8a6cc43f171c069f
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-05-16 05:39:49 +0800

    feat: upload profile image

--- client/src/Pages/UserProfile/UpdateProfileImage.tsx
+++ client/src/Pages/UserProfile/UpdateProfileImage.tsx
@@ -1,27 +1,48 @@
 import React, { useEffect } from 'react'
 import { useFormik } from 'formik'
 import { Button, Avatar, Chip } from '@mui/material'
+
+// Hooks
+import useUser from 'src/Hooks/useUser';
+
+import useFirebase from 'src/Hooks/useFirebase';
 interface PropsType {
   closeModal:() => void
   data: any
 }
 export default function UpdateProfileImage({closeModal, data}:PropsType) {
+  const {editInfo} = useUser();
+  const {uploadFile} = useFirebase();
+
   const UpdateImage = useFormik({
     initialValues: {
       image: data[0]?.image || ''
     },
     onSubmit: values => {
+      console.log(values.image)
+      editInfo({
+        firstName: data[0]?.name?.first || '',
+        middleName: data[0]?.name?.middle || '',
+        lastName: data[0]?.name?.last || '',
+        suffixName: data[0]?.name?.suffixName || '',
+        contact: data[0]?.contact || '',
+        photo: values.image
+      })
       closeModal()
     }
   })
   return <>
     <div className='grow flex flex-col justify-center items-center'>
       <Avatar variant="circular" src={UpdateImage.values.image} alt="sample" sx={{width:"150px",height:"150px"}} />
-      <input type="file" id='image' name="image" accept=".png, .jpg"  onChange={(e:any)=>{
-        if(e.target.files.length > 0){ 
-          UpdateImage.setFieldValue(`image`,URL.createObjectURL(e.target.files[0]))
+      <input type="file" id='image' name="image" accept=".png, .jpg"
+      onChange={async (e)=>{
+        if(e.target.files) {
+          const fileUrl = await uploadFile(e.target.files[0], 'events_place_marikina');
+          console.log(fileUrl)
+          UpdateImage.setFieldValue("image",fileUrl)
         }
-      }} className='hidden'/>
+      }}
+      className='hidden'/>
       <label htmlFor="image" className='mt-[2em]'>
         <Chip label="Upload Image" onClick={()=>{}} />
       </label>
