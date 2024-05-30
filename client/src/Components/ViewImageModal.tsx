9d5afee1473c1a33fd13d6a07ad5439fc522f0b8
Harold Graphs <haroldcastillo272@gmail.com>
2024-05-30 20:26:38 +0800

    fix: view image modal

--- client/src/Components/ViewImageModal.tsx
+++ client/src/Components/ViewImageModal.tsx
@@ -5,7 +5,35 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 export default function ViewImageModal({data,index}:{data:any,index:number}) {
   const [counter, setCounter] = React.useState(index)
   return (
-    <div className='w-[90vw] h-[80vh] rounded-xl' style={{background:`url("${data[counter]}") no-repeat`,backgroundSize:"cover",backgroundPosition:"center"}}>
+    // <div className='w-[90vw] h-[80vh] rounded-xl' style={{background:`url("${data[counter]}") no-repeat`,backgroundSize:"cover",backgroundPosition:"center"}}>
+    //   <IconButton aria-label="" sx={{position:"absolute",top:"50%",left:"-40px",transform:"translateY(-50%)"}} 
+    //     onClick={()=>{
+    //       if(counter !==0){
+    //         setCounter(counter-1)
+    //       }else{
+    //         setCounter(data.length-1)
+    //       }
+    //     }}
+    //   >
+    //     <div className='relative rounded-full bg-[white] w-[70px] h-[70px]'>
+    //       <NavigateBeforeIcon sx={{fontSize:"30px",position:"absolute",top:"50%",left:"54%", transform:"translate(-50%,-50%)"}}/>
+    //     </div>
+    //   </IconButton>
+    //   <IconButton aria-label="" sx={{position:"absolute",top:"50%",right:"-40px",transform:"translateY(-50%)"}} 
+    //     onClick={()=>{
+    //       if(counter !==data.length-1){
+    //         setCounter(counter+1)
+    //       }else{
+    //         setCounter(0)
+    //       }
+    //     }}
+    //   >
+    //     <div className='relative rounded-full bg-[white] w-[70px] h-[70px]'>
+    //       <NavigateNextIcon sx={{fontSize:"30px",position:"absolute",top:"50%",left:"54%", transform:"translate(-50%,-50%)"}}/>
+    //     </div>
+    //   </IconButton>
+    // </div>
+    <div className='w-[60vw] h-[80vh] bg-[black] rounded-xl overflow-hidden' >
       <IconButton aria-label="" sx={{position:"absolute",top:"50%",left:"-40px",transform:"translateY(-50%)"}} 
         onClick={()=>{
           if(counter !==0){
@@ -16,7 +44,7 @@ export default function ViewImageModal({data,index}:{data:any,index:number}) {
         }}
       >
         <div className='relative rounded-full bg-[white] w-[70px] h-[70px]'>
-          <NavigateBeforeIcon sx={{fontSize:"30px",position:"absolute",top:"50%",left:"54%", transform:"translate(-50%,-50%)"}}/>
+          <NavigateBeforeIcon sx={{fontSize:"30px",position:"absolute",top:"50%",left:"49%", transform:"translate(-50%,-50%)"}}/>
         </div>
       </IconButton>
       <IconButton aria-label="" sx={{position:"absolute",top:"50%",right:"-40px",transform:"translateY(-50%)"}} 
@@ -32,6 +60,7 @@ export default function ViewImageModal({data,index}:{data:any,index:number}) {
           <NavigateNextIcon sx={{fontSize:"30px",position:"absolute",top:"50%",left:"54%", transform:"translate(-50%,-50%)"}}/>
         </div>
       </IconButton>
+      <img src={data[counter]} alt="" className='w-full h-full object-contain'/>
     </div>
   )
 }
