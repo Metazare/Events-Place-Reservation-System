a20086c6fe8b9ba08d0926f2cfe0ded5da0ae737
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2024-05-31 18:48:04 +0800

    feat: my listing to only display owned events place

--- client/src/Pages/EventsPlace/MyListings.tsx
+++ client/src/Pages/EventsPlace/MyListings.tsx
@@ -159,11 +159,13 @@ export default function MyListings() {
   )
 }
 
-
 function EventCardList({isHost,data,edit,setData}: {isHost:boolean,data:any,edit:boolean,setData:any}){
+  const {authUser} = useAuthContext();
+  const filteredData = data.filter((item: any) => item.host.userId === (authUser?.userId));
+
   return<>
     <div className={`grid  mb-7`} style={isHost && !(isHost && edit)?{gridTemplateColumns:"repeat(auto-fill, minmax(250px, 1fr))",gap:"1em"}:{gridTemplateColumns:"repeat(auto-fill, minmax(200px, 1fr))",gap:"1.5em"}}>
-      {data.map((data:any,index:any)=>(
+      {filteredData.map((data:any,index:any)=>(
         isHost?
           edit  
             ? <EventCard key={data._id} data={data} type="manage"/>
