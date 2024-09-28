d6cc6021e2afd84837a07edba6914528be8aebe3
Dianne Chrystalin Brandez <chrystarin@gmail.com>
2024-09-28 19:32:17 +0800

    fix: logout bug

--- client/src/Hooks/useAuth.tsx
+++ client/src/Hooks/useAuth.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../Context/AuthContext";
-import axios from './useAxios';
+import axios from "./useAxios";
 
 interface RegisterData {
     firstName: string;
@@ -43,30 +43,26 @@ const useLogin = () => {
     const login = async (data: LoginData) => {
         const { email, password } = data;
 
-        const success = handleInputErrors({email, password});
+        const success = handleInputErrors({ email, password });
         if (!success) return;
 
         setLoading(true);
 
         try {
-            await axios
-                .post(`/auth/login`, data)
-                .then((response: any) => {
-                    localStorage.setItem('user', JSON.stringify(response.data))
-                    setAuthUser(response.data);
-                    console.log(response.data)
-                    if (response.data.name.first === "Admin"){
-                        console.log("This is admin account!")
-                    }
-                    else if (response.data.license){
-                        localStorage.setItem('mode', 'Host');
-                        navigate('/profile');
-                    }
-                    else {
-                        localStorage.setItem('mode', 'Renter');
-                        navigate('/profile');
-                    }
-                });
+            await axios.post(`/auth/login`, data).then((response: any) => {
+                localStorage.setItem("user", JSON.stringify(response.data));
+                setAuthUser(response.data);
+                console.log(response.data);
+                if (response.data.name.first === "Admin") {
+                    console.log("This is admin account!");
+                } else if (response.data.license) {
+                    localStorage.setItem("mode", "Host");
+                    navigate("/profile");
+                } else {
+                    localStorage.setItem("mode", "Renter");
+                    navigate("/profile");
+                }
+            });
         } catch (error: any) {
             toast.error(error.response?.data?.message);
         } finally {
@@ -74,27 +70,24 @@ const useLogin = () => {
         }
     };
 
-
     const loginAdmin = async (data: LoginData) => {
         const { email, password } = data;
 
-        const success = handleInputErrors({email, password});
+        const success = handleInputErrors({ email, password });
         if (!success) return;
 
         setLoading(true);
 
         try {
-            await axios
-                .post(`/auth/login`, data)
-                .then((response: any) => {
-                    localStorage.setItem('user', JSON.stringify(response.data))
-                    setAuthUser(response.data);
-                    console.log(response.data)
-                    if (response.data.name.first === "Admin"){
-                        navigate('/admin/renters');
-                        localStorage.setItem('mode', 'Admin');
-                    }
-                });
+            await axios.post(`/auth/login`, data).then((response: any) => {
+                localStorage.setItem("user", JSON.stringify(response.data));
+                setAuthUser(response.data);
+                console.log(response.data);
+                if (response.data.name.first === "Admin") {
+                    navigate("/admin/renters");
+                    localStorage.setItem("mode", "Admin");
+                }
+            });
         } catch (error: any) {
             toast.error(error.response?.data?.message);
         } finally {
@@ -111,16 +104,17 @@ const useLogout = () => {
     const logout = async () => {
         setLoading(true);
         try {
-            await axios
-                .post(`/auth/logout`)
-                .then((response: any) => {
-                    localStorage.removeItem("user");
-                    localStorage.removeItem('New Host');
-                    setAuthUser(null);
-                });
+            await axios.post(`/auth/logout`).then((response: any) => {
+                localStorage.removeItem("user");
+                localStorage.removeItem("New Host");
+                setAuthUser(null);
+            });
         } catch (error: any) {
-            toast.error(error.response?.data?.message);
+            // toast.error(error.response?.data?.message);
         } finally {
+            localStorage.removeItem("user");
+            localStorage.removeItem("New Host");
+            setAuthUser(null);
             setLoading(false);
         }
     };
@@ -128,7 +122,7 @@ const useLogout = () => {
 };
 
 const useRegister = () => {
-    const { login } = useLogin(); 
+    const { login } = useLogin();
     const [loading, setLoading] = useState(false);
     const { setAuthUser } = useAuthContext();
     const navigate = useNavigate();
@@ -136,7 +130,9 @@ const useRegister = () => {
     const isEmailUnique = async (data): Promise<boolean> => {
         setLoading(true);
         try {
-            const response = await axios.post(`/auth/checkemail`, {email: data});
+            const response = await axios.post(`/auth/checkemail`, {
+                email: data,
+            });
             return !response.data.duplicateEmail;
         } catch (error: any) {
             toast.error("Email already exists");
@@ -149,17 +145,18 @@ const useRegister = () => {
     const register = async (data: RegisterData) => {
         setLoading(true);
         try {
-            await axios.post(`/auth/register`, data).then((response: any) => {
-                login({ email: data.email, password: data.password });
-                localStorage.setItem("user", JSON.stringify(response.data));
-                setAuthUser(response.data);
-                navigate('/profile');
-            })
-            .catch((error: any) => {
-                console.log(error)
-                toast.error(error.response?.data?.message);
-            }
-            );
+            await axios
+                .post(`/auth/register`, data)
+                .then((response: any) => {
+                    login({ email: data.email, password: data.password });
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                    setAuthUser(response.data);
+                    navigate("/profile");
+                })
+                .catch((error: any) => {
+                    console.log(error);
+                    toast.error(error.response?.data?.message);
+                });
         } catch (error: any) {
             toast.error(error.response?.data?.message);
         } finally {
@@ -170,15 +167,16 @@ const useRegister = () => {
     const registerHost = async (data: RegisterHostData) => {
         setLoading(true);
         try {
-            await axios.post(`/auth/register/host`, data)
-                        .then((response: any) => {
-                            console.log(response)
-                            toast.success("Host registration successful");
-                        })
-                        .catch((error: any) => {
-                            console.log(error)
-                            toast.error(error.response?.data?.message);
-                        });
+            await axios
+                .post(`/auth/register/host`, data)
+                .then((response: any) => {
+                    console.log(response);
+                    toast.success("Host registration successful");
+                })
+                .catch((error: any) => {
+                    console.log(error);
+                    toast.error(error.response?.data?.message);
+                });
         } catch (error: any) {
             toast.error(error.response?.data?.message);
         } finally {
@@ -197,7 +195,7 @@ const usePasswordReset = () => {
         setLoading(true);
         try {
             await axios
-                .post(`/user/password/forgot`, {email:email})
+                .post(`/user/password/forgot`, { email: email })
                 .then((response: any) => {
                     toast.success("Password reset link sent to email");
                 });
@@ -214,12 +212,12 @@ const usePasswordReset = () => {
             await axios
                 .post(`/user/password/reset`, {
                     password: password,
-                    hash: hash
+                    hash: hash,
                 })
                 .then((response: any) => {
                     toast.success("Password reset successful");
                     setTimeout(() => {
-                        navigate('/login');
+                        navigate("/login");
                     }, 2000);
                 });
         } catch (error: any) {
@@ -230,30 +228,43 @@ const usePasswordReset = () => {
     };
 
     return { loading, forgetPassword, resetPassword };
-}
+};
 
 const ProtectedRoute = ({ allowedRoles }) => {
     const { authUser } = useAuthContext();
-    return (
-        authUser
-        ? allowedRoles 
-            ? allowedRoles?.includes(localStorage.getItem('mode'))
-                ? <Outlet/>
-                : <Navigate to="/forbidden"/>
-            : <Outlet/> 
-        : <Navigate to="/login"/>
+    return authUser ? (
+        allowedRoles ? (
+            allowedRoles?.includes(localStorage.getItem("mode")) ? (
+                <Outlet />
+            ) : (
+                <Navigate to="/forbidden" />
+            )
+        ) : (
+            <Outlet />
+        )
+    ) : (
+        <Navigate to="/login" />
     );
 };
 
 const PublicRoute = () => {
     const { authUser } = useAuthContext();
-    return (
-        authUser
-        ? localStorage.getItem('mode') === "Admin" 
-            ? <Navigate to="/admin/renters"/> 
-            : <Navigate to="/"/>
-        : <Outlet/> 
+    return authUser ? (
+        localStorage.getItem("mode") === "Admin" ? (
+            <Navigate to="/admin/renters" />
+        ) : (
+            <Navigate to="/" />
+        )
+    ) : (
+        <Outlet />
     );
 };
 
-export { useLogin, useLogout, useRegister, usePasswordReset, ProtectedRoute, PublicRoute };
+export {
+    useLogin,
+    useLogout,
+    useRegister,
+    usePasswordReset,
+    ProtectedRoute,
+    PublicRoute,
+};
