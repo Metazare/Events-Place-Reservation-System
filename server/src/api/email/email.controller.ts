01533c893e076e69f69d4951eee22788e4060eab
Dianne Chrystalin Brandez <chrystarin@gmail.com>
2024-11-23 08:06:01 +0800

    fix: nodemailer function

--- server/src/api/email/email.controller.ts
+++ server/src/api/email/email.controller.ts
@@ -1,36 +1,45 @@
-import { BodyRequest, RequestHandler } from 'express';
-import { Email } from './email.types';
-import envs from '../../utilities/envs';
+import { BodyRequest, RequestHandler } from "express";
+import { Email } from "./email.types";
+import envs from "../../utilities/envs";
 
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
 
 const { NODEMAILER_EMAIL, NODEMAILER_PASSWORD } = envs;
 const transporter = nodemailer.createTransport({
-    service: 'outlook',
+    service: "gmail",
     auth: {
         user: NODEMAILER_EMAIL,
-        pass: NODEMAILER_PASSWORD
-    }
+        pass: NODEMAILER_PASSWORD,
+    },
 });
 
-export const createEmail = (to: string, subject: string, content: string): Promise<unknown> =>
+export const createEmail = (
+    to: string,
+    subject: string,
+    content: string
+): Promise<unknown> =>
     transporter.sendMail({
-        from: NODEMAILER_EMAIL, // Sender address
+        from: `"noreply" <${NODEMAILER_EMAIL}>`, // Sender address
         to: to, // Recipient address
         subject: subject, // Subject line
-        html: content // HTML body
+        html: content, // HTML body
     });
 
-export const sendEmail: RequestHandler = async (req: BodyRequest<Email>, res) => {
+export const sendEmail: RequestHandler = async (
+    req: BodyRequest<Email>,
+    res
+) => {
     const { to, subject, content } = req.body;
 
     try {
         // Send email
         res.json(await createEmail(to, subject, content));
     } catch (error: any) {
-        console.error('Error sending email: ', error.message);
+        console.error("Error sending email: ", error.message);
         // General error response
-        res.status(500).json({ error: 'Error sending email', message: error.message });
+        res.status(500).json({
+            error: "Error sending email",
+            message: error.message,
+        });
     }
 };
