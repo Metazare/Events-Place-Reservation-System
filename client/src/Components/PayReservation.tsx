7f79da6e95d7714d9b3c8401edf76af022ac0633
Harold Graphs <haroldcastillo272@gmail.com>
2024-12-27 15:05:18 +0800

    fix: reservation form

--- client/src/Components/PayReservation.tsx
+++ client/src/Components/PayReservation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { formatToMoney } from "src/Utils/utils";
 export default function PayReservation({
     closeModal,
     accountNum,
@@ -13,8 +13,14 @@ export default function PayReservation({
         <div className="p-4 w-[95vw] max-w-[400px]">
             <p className="text-[20px] font-semibold">Pay Reservation</p>
             <p className="mt-4 text-[14px] text-[#144273]">
-                Pay your reservation total of ₱{total} through GCash at{" "}
-                {accountNum}
+                {/* Pay your reservation total of ₱{total} through GCash at{" "}
+                {accountNum} */}
+                Your total reservation is ₱{formatToMoney(total)}. but you are
+                required only to pay 50% of the total amount to secure your
+                reservation. The remaining balance will be paid on the day of
+                the event. Please pay the amount of ₱{formatToMoney(total / 2)}{" "}
+                to secure your reservation. You can pay through GCash at{" "}
+                {accountNum}.
             </p>
             <p className="mt-4 text-[14px] text-[#144273]">
                 Use the link below to pay
--- client/src/Pages/EventsPlace/ReservationForm.tsx
+++ client/src/Pages/EventsPlace/ReservationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import TextField from "src/Components/TextField";
 import { useFormik } from "formik";
 import AmenitiesCard from "src/Components/AmenitiesCard";
